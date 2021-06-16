f3556e52eedba64310e784c7c4990cb5ad965023
Alexey Izhendeev <takedo19@gmail.com>
2021-06-16 15:45:12 +0700

    First commit. Started project, made serializer, posts, pagination.

---
+++ api/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, TagDetailView
+
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('tags/<slug:tag_slug>/', TagDetailView.as_view()),
+]
---
+++ api/core/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions, pagination, generics
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.response import Response
+from taggit.models import Tag
+
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
+
+
+class TagDetailView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    pagination_class = PageNumberSetPagination
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug'].lower()
+        tag = Tag.objects.get(slug=tag_slug)
+        return Post.objects.filter(tags=tag)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination
