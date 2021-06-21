947e5f6edfda2009b5252922fefacc21568d8cb6
Alexey Izhendeev <takedo19@gmail.com>
2021-06-21 16:23:14 +0700

    Finished api

--- api/core/admin.py
+++ api/core/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
-admin.site.register(Post, PostAdmin)
+admin.site.register(Post, PostAdmin, )
 
 
--- api/core/views.py
+++ api/core/views.py
@@ -1,9 +1,44 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions, pagination, generics
-from .models import Post
-from .serializers import PostSerializer
+from rest_framework import viewsets, permissions, pagination, generics, filters
+from .models import Post, Comment
+from .serializers import PostSerializer, TagSerializer, RegisterSerializer, UserSerializer, CommentSerializer
 from rest_framework.response import Response
 from taggit.models import Tag
+
+
+class CommentView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        post_slug = self.kwargs['post_slug'].lower()
+        post = Post.objects.get(slug=post_slug)
+        return Comment.objects.filter(post=post)
+
+
+class RegisterView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+            'message': 'User successfully created',
+        })
+
+
+class ProfileView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'user': UserSerializer(request.user, context=self.get_serializer_context()).data,
+        })
 
 
 class PageNumberSetPagination(pagination.PageNumberPagination):
@@ -23,9 +58,23 @@ class TagDetailView(generics.ListAPIView):
         return Post.objects.filter(tags=tag)
 
 
+class TagView(generics.ListAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.AllowAny]
+
+
 class PostViewSet(viewsets.ModelViewSet):
+    search_fields = ['content', 'h1']
+    filter_backends = (filters.SearchFilter,)
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
     pagination_class = PageNumberSetPagination
+
+
+class AsideView(generics.ListAPIView):
+    queryset = Post.objects.all().order_by('-id')[:5]
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
